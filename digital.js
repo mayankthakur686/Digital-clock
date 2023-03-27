aea10435716704ef719584bb16edb254c0d2110e
mayankthakur686 <102917792+mayankthakur686@users.noreply.github.com>
2023-03-27 23:43:39 +0530

    Add files via upload

---
+++ digital.js
@@ -0,0 +1,32 @@
+myclock = (()=> {
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+
+    if(hrs >=12){
+        session.innerHTML = 'PM';
+    }
+    else{
+        session.innerHTML = 'AM';
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}) 
+setInterval(myclock,10);
+
+function changeto12(){
+    
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+}
+function changeto24(){
+    
+    if(hrs > 12){
+        hrs = hrs + 12;
+    }
+}
